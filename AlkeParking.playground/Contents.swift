87f09ebcde726082d276b770aeb9e496b8ad25cb
Thayse Moreira <thayse.moreira@mercadolivre.com>
2022-06-14 18:31:32 -0300

    Conclusion exercise 2

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -1,14 +1,14 @@
 import UIKit
 
 enum VehicleType {
-    case carro, motocicleta, microonibus, onibus
+    case car, motorcycle, microbus, bus
     
     var tarifa: Int {
         switch self {
-            case .carro: return 20
-            case .motocicleta: return 15
-            case .microonibus: return 25
-            case .onibus: return 30
+            case .car: return 20
+            case .motorcycle: return 15
+            case .microbus: return 25
+            case .bus: return 30
         }
     }
 }
@@ -24,7 +24,7 @@ protocol Parkable {
 struct Vehicle: Parkable, Hashable {
     let plate: String
     let vehicleType: VehicleType
-    let checkInTime: Date = Date()
+    let checkInTime: Date
     var discountCard: String?
     var parkedTime: Int {
         get { Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0 }
@@ -43,6 +43,39 @@ struct Vehicle: Parkable, Hashable {
 
 struct Parking{
     var vehicles: Set<Vehicle> = Set()
+    
+    
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish:
+    (Bool) -> Void) {
+
+    }
 }
 
-let alkeParking: Parking = Parking()
+var alkeParking = Parking()
+
+let car = Vehicle(plate: "AA111AA", vehicleType: .car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+
+let moto = Vehicle(plate: "B222BBB", vehicleType: .motorcycle, checkInTime: Date(), discountCard: nil)
+
+let miniBus = Vehicle(plate: "CC333CC", vehicleType: .microbus, checkInTime: Date(), discountCard:
+nil)
+
+let bus = Vehicle(plate: "DD444DD", vehicleType: .bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_002")
+
+alkeParking.vehicles.insert(car)
+
+alkeParking.vehicles.insert(moto)
+
+alkeParking.vehicles.insert(miniBus)
+
+alkeParking.vehicles.insert(bus)
+
+let car1 = Vehicle(plate: "AA111AA", vehicleType: .car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+
+let car2 = Vehicle(plate: "AA111AA", vehicleType: .car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_003")
+
+print(alkeParking.vehicles.insert(car).inserted)
+
+print(alkeParking.vehicles.insert(car2).inserted)
+
+alkeParking.vehicles.remove(moto)
